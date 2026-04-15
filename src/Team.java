825e84560fff2d13cf48e343c64047f4a53fc6cd
YL626 <rauldelira40@gmail.com>
2026-04-15 17:23:52 -0600

    If applied this commit will introduce the team class with a constructor, and the container of members, as well as starting the total team skill score

--- src/Team.java
+++ src/Team.java
@@ -1,3 +1,15 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Team {
+
+    private int totalTeamSkillScore = 0;
+
+    private List<Student> members = new ArrayList<>();
+
+    public void Team(Student leader) {
+        totalTeamSkillScore += leader.getTotalScore();
+        members.add(leader);
+    }
     
 }
