d2ff8ec9817fbea455d468e85a35331dee207924
kacperm37 <kacpermazur024@gmail.com>
2026-04-15 17:14:06 -0600

    Created files for Student, Team, and Formation to handle each student, team and formation of it all

---
+++ src/Student.java
@@ -0,0 +1,24 @@
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private boolean checkedIn;
+    private int compSciNum;
+    private int familiarML;
+    private int expGIS;
+    private int expAWS;
+    private int totalScore;
+
+    public Student(String ID){
+        firstName = "";
+        lastName = "";
+        email = "";
+        checkedIn = false;
+        compSciNum = 0;
+        familiarML = 0;
+        expGIS = 0;
+        expAWS = 0;
+        totalScore = 0;
+
+    }
+}
