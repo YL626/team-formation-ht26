1c99c493e9d22aebf630f1d2c4a9c8c8e64ad050
YL626 <rauldelira40@gmail.com>
2026-04-15 17:47:03 -0600

    If applied this commit will add more functionality to the Team class

--- src/Team.java
+++ src/Team.java
@@ -1,5 +1,7 @@
 import java.util.List;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Team {
 
@@ -10,6 +12,19 @@ public class Team {
     public void Team(Student leader) {
         totalTeamSkillScore += leader.getTotalScore();
         members.add(leader);
+    }
+
+    public void addTeamMember(Student member){
+        totalTeamSkillScore += member.getTotalScore();
+        members.add(member);
+    }
+
+    public void showTeamMembers(){
+        Array[String] membersOfTeam = new Array[5];
+        for (Student member : members){
+            membersOfTeam.add(member.getFirstName() + " " + member.getLastName() + " " + member.get);
+        }
+        Arrays.toString(membersOfTeam);
     }
     
 }
