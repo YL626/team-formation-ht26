9f072cc0c9a1fdca33ee586bc27cd4e2bccce5c6
kacperm37 <kacpermazur024@gmail.com>
2026-04-15 18:08:34 -0600

    wrote all getters and setters for Student, made a test file for... testing?

--- src/Student.java
+++ src/Student.java
@@ -1,8 +1,7 @@
 public class Student {
+    private String ID;
     private String firstName;
     private String lastName;
-    private String email;
-    private boolean checkedIn;
     private int compSciNum;
     private int familiarML;
     private int expGIS;
@@ -10,10 +9,9 @@ public class Student {
     private int totalScore;
 
     public Student(String ID){
+        this.ID = ID;
         firstName = "";
         lastName = "";
-        email = "";
-        checkedIn = false;
         compSciNum = 0;
         familiarML = 0;
         expGIS = 0;
@@ -22,26 +20,67 @@ public class Student {
 
     }
 
+    //ID
+    public void setID(String ID){
+        this.ID = ID;
+    }
+    public String getID(){
+        return ID;
+    }
+
+    //First Name
     public void setFirstName(String firstName){
         this.firstName = firstName;
     }
-
     public String getFirstName(){
         return firstName;
     }
 
+    //Last Name
     public void setLastName(String lastName){
         this.lastName = lastName;
     }
-
     public String getLastName(){
         return lastName;
     }
 
-    public void setTotalScore(){
-        totalScore = compSciNum + familiarML + expGIS + expAWS;
+    //Highest Level Comp Sci class taken
+    public void setCompSciNum(int compSciNum){
+        this.compSciNum = compSciNum;
+        totalScore += compSciNum;
+    }
+    public int getCompSciNum(){
+        return compSciNum;
     }
 
+    //Familiar Machine Learning
+    public void setFamilarML(int familiarML){
+        this.familiarML = familiarML;
+        totalScore += familiarML;
+    }
+    public int getFamiliarML(){
+        return familiarML;
+    }
+
+    //Experience with GIS
+    public void setExpGIS(int expGIS){
+        this.expGIS = expGIS;
+        totalScore += expGIS;
+    }
+    public int getExpGIS(){
+        return expGIS;
+    }
+
+    //Experience with AWS
+    public void setExpAWS(int expAWS){
+        this.expAWS = expAWS;
+        totalScore += expAWS;
+    }
+    public int getExpAWS(){
+        return expAWS;
+    }
+
+    //Student Score
     public int getTotalScore(){
         return totalScore;
     }
