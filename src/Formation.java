fbe137229852c33dcdd316d11ffc464f24beec38
YL626 <rauldelira40@gmail.com>
2026-04-15 18:26:20 -0600

    If applied this commit will finish team object functionality and organization. As well as begin the input reading process for Formation.java

--- src/Formation.java
+++ src/Formation.java
@@ -1,3 +1,46 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+//import org.apache.commons.csv.CSVFormat;
+//import org.apache.commons.csv.CSVRecord;
+
 public class Formation {
+
+    private static String inputFile;
+
+
+    /*private static void intakeInputInfo(String filename) throws IOException{
+        Reader inputReader = new FileReader(filename);
+        Iterable<CSVRecord> entries = CSVFormat.DEFAULT.parse(inputReader);
+        for (CSVRecord entry: entries){
+            entryReader(entry);
+        }
+        inputReader.close();
+    } */
+
+    private static void formationRunner(String[] args) throws FileNotFoundException, IOException{
+        inputFile = args[0];
+
+    }
+
+
+    public static void main(String[] args){
+        try {
+            formationRunner(args);
+        } catch (FileNotFoundException noFile){
+            System.err.println("Error: " + noFile.getMessage());
+            return;
+        } catch (IOException ioException){
+            System.err.println("Error: " + ioException.getMessage());
+            return;
+        } catch (NumberFormatException wrongNum){
+            System.err.println("Error: " + wrongNum.getMessage());
+            return;
+        } catch (IllegalArgumentException wrongArg){
+            System.err.println("Error: " + wrongArg.getMessage());
+            return;
+        }
+    }
     
 }
--- src/Team.java
+++ src/Team.java
@@ -1,30 +1,36 @@
 import java.util.List;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Team {
 
     private int totalTeamSkillScore = 0;
-
+    private int teamSize = 0;
     private List<Student> members = new ArrayList<>();
+
 
     public void Team(Student leader) {
         totalTeamSkillScore += leader.getTotalScore();
         members.add(leader);
+        teamSize++;
     }
 
     public void addTeamMember(Student member){
         totalTeamSkillScore += member.getTotalScore();
         members.add(member);
+        teamSize++;
     }
 
     public void showTeamMembers(){
-        Array[String] membersOfTeam = new Array[5];
-        for (Student member : members){
-            membersOfTeam.add(member.getFirstName() + " " + member.getLastName() + " " + member.get);
+        String[] membersOfTeam = new String[5];
+        for (int i = 0; i < teamSize; i++){
+            membersOfTeam[i] = members.get(i).getFirstName() + " " + members.get(i).getLastName() + " " + members.get(i).getID();
         }
         Arrays.toString(membersOfTeam);
+    }
+
+    public int size(){
+        return teamSize;
     }
     
 }
