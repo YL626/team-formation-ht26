b29d035e10c3e6fd790faa0205c41011e9fe1909
YL626 <rauldelira40@gmail.com>
2026-04-17 02:27:05 -0600

    If applied this commit will finish implementing the data intake process, creating a list of students all with their answers and skill scores recorded. This commit will also introduce the display of team amounts and relevant data such as leftover students

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # team-formation-ht26
 Team formation script for DevNet 2026 Spring Hackathon 
+
+Please note, when getting the score value from CSV entries we compare the beginning of each possible answer as these are multiple choice questions with set responses.
+
+What is your level of familiarity with machine learning (ML)? 
+
+Advanced : 3 
+Intermediate : 2 
+Beginner : 1 
+Not experienced at all : 0 
+
+What is the highest-level computer science course you have completed? 
+
+500+ level course: 5 
+400 level course: 4  
+300 level course: 3 
+200 level course: 2 
+100 level course: 1 
+None: 0 
+
+Do you have experience working with geospatial data or GIS Applications? 
+Yes: 3 No: 0 
+
+Do you have experience working with Amazon Web Services (i.e. ECS, S3 Buckets) 
+Yes : 2  No : 0 
--- src/main/java/Formation.java
+++ src/main/java/Formation.java
@@ -10,13 +10,15 @@ import org.apache.commons.csv.CSVRecord;
 
 public class Formation {
 
-    private static String inputFile;
-    private static int totalTeams;
+    private static int numOfTeams;
+
     private static int totalStudents = 0;
     private static int maxStudents = 108;
 
     private static Team[] teams;
     private static ArrayList<Student> allStudents = new ArrayList<>();
+
+    private static boolean teamWithThreeNeeded = false;
 
 
     private static void intakeInputInfo(String filename) throws IOException{
@@ -32,11 +34,49 @@ public class Formation {
     } 
 
     private static void findNumOfTeams(){
-
+        numOfTeams = totalStudents / 4;
+        int remaining = totalStudents % 4;
+        switch (remaining) {
+            case 0:
+                System.out.println("Tremendous, no leftover students!");
+                System.out.println(numOfTeams);
+                break;
+            case 3:
+                numOfTeams++;
+                teamWithThreeNeeded = true;
+                System.out.println("One team of 3 must be made");
+                System.out.println(numOfTeams);
+                break;
+            default:
+                System.out.println("Warning: " + remaining + " students are leftover, please assign them a team manually.");
+                System.out.println(numOfTeams);
+                break;
+        }
     }
 
-    private static void studentSetup(){
+    private static int compSciLevelScore(String compSciLevel){
+        Character level = compSciLevel.charAt(0);
+        return Character.isDigit(level) ? Character.getNumericValue(level) : 0;
+    }
 
+    private static int mlFamiliarityScore(String mlFamiliarity){
+        Character familiarity = mlFamiliarity.charAt(0);
+        return switch (familiarity) {
+            case 'A' -> 3;
+            case 'I' -> 2;
+            case 'B' -> 1;
+            default -> 0;
+        };
+    }
+
+    private static int gisScore(String xpGIS){
+        Character responseSignal = xpGIS.charAt(0);
+        return responseSignal.equals('Y') ? 3 : 0;
+    }
+
+    private static int awsScore(String xpAWS){
+        Character responseSignal = xpAWS.charAt(0);
+        return responseSignal.equals('Y') ? 2 : 0;
     }
 
     private static void entryReader(CSVRecord entry){
@@ -48,15 +88,23 @@ public class Formation {
         String xpGIS = entry.get(15).trim();
         String xpAWS = entry.get(16).trim();
 
+        Student currentStudent = new Student(studentID);
 
-
+        currentStudent.setFirstName(firstName);
+        currentStudent.setLastName(lastName);
+        currentStudent.setCompSciNum(compSciLevelScore(compSciLevel));
+        currentStudent.setFamilarML(mlFamiliarityScore(mlFamiliarity));
+        currentStudent.setExpGIS(gisScore(xpGIS));
+        currentStudent.setExpAWS(awsScore(xpAWS));
+        allStudents.add(currentStudent);
+        totalStudents++;
     }
 
 
     private static void formationRunner(String[] args) throws FileNotFoundException, IOException{
         String inputFile = args[0];
         intakeInputInfo(inputFile);
-        //intake and create students
+        findNumOfTeams();
         
     }
 
